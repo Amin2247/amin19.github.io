8d4300bd6fdf69acb94287d87fae255d5859f1cd
Amin2247 <101484972+Amin2247@users.noreply.github.com>
2022-04-13 16:27:02 -0700

    Update sketchPresidents.js

--- p5/sketchPresidents.js
+++ p5/sketchPresidents.js
@@ -21,6 +21,14 @@ function setup() {
   //index =int(random(5));
   console.log(index);
   mouseClicked(buttonPressed);
+square(110, 180, 185);
+triangle(70, 180, 330, 180, 200, 75);
+fill(0, 0, 255)
+square(120, 190, 50);
+square(240, 190, 50);
+fill(53, 0, 0)
+rect(180, 280, 50, 75);
+
 }
 function mouseClicked(){
   buttonPressed = true;
@@ -33,4 +41,3 @@ function draw() {
    buttonPressed = false;
   }
 }
-
